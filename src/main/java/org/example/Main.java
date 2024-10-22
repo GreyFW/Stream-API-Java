080f3a1557c35b9ca503e3bbe799bb0e1427bf92
GreyF <greysforgettea@gmail.com>
2024-10-23 03:09:39 +0700

    task 2 resolved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.List;
-import java.util.Collections.*;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,22 +18,37 @@ public class Main {
 
         List<Visitor> visitors;
         try (FileReader reader = new FileReader(path)) {
-            Type visitorListType = new TypeToken<List<Visitor>>() {
-            }.getType();
+            Type visitorListType = new TypeToken<List<Visitor>>(){}.getType();
             visitors = gson.fromJson(reader, visitorListType);
         } catch (IOException exception) {
             System.err.println(exception.getMessage());
             return;
         }
 
+
+
         System.out.println("\n\tЗАДАНИЕ 1:\nВывести список посетителей и их кол-во.\n");
         long visitorsCount = visitors.size();
         for (Visitor visitor : visitors) {
-            System.out.println(visitor + "\n");
+            // System.out.println(visitor + "\n");
         }
         System.out.println("Кол-во посетителей: " + visitorsCount);
 
+
+
         System.out.println("\n\tЗАДАНИЕ 2:\nСписок и кол-во книг из избранного без повт.\n");
+        Set<Book> books;
+        books = visitors
+                .stream() // закидываем всё в поток
+                .flatMap(visitor -> visitor.getFavoriteBooks().stream())
+                // вытаскиваем список книг из каждого элемеента потока (посетителя)
+                .collect(Collectors.toSet()); // преобразуем результат потока в мн-во
+
+        // System.out.println(books);
+
+
+
+        //
 
 
     }
